2f9c4126306cda25957bec12554c7162fa20e22e
Nycolas Felipe <nycolaslourenco.contato@gmail.com>
2022-11-04 16:27:27 -0300

    Alteração tela de login

--- client/src/pages/Home/styles.jsx
+++ client/src/pages/Home/styles.jsx
@@ -1,5 +1,41 @@
 import styled from 'styled-components';
 
+const mainColor = "#005E93";
+
+
+export const Main = styled.div`
+  display: flex;
+  justify-content: center;
+  background: #FAFAFA;
+`;
+
+export const ContainerLeft = styled.div`
+  flex: 1;
+  background-image: url('../../img/backgroundMain.jpg');
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-color: #FAFAFA;
+  height: 100vh;
+
+  @media screen and (max-width: 900px) {
+    background-image: none;
+    flex: none;
+  }
+`;
+
+export const ContainerRight = styled.div`
+  background: #eee;
+  width: 400px;
+  height: 100vh;
+  right: 0;
+
+  @media screen and (max-width: 900px) {
+    width: 100vw;
+    background: #FAFAFA;
+  }
+`;
+
 export const Header = styled.h1`
   margin: 0 auto;
   padding-top: 40px;
@@ -18,6 +54,8 @@ export const Title = styled.h1`
   font-size: 5rem;
   font-weight: 600;
   margin: 0;
+  color: ${mainColor};
+  user-select: none;
 `;
 
 export const Subtitle = styled.h1`
@@ -26,15 +64,19 @@ export const Subtitle = styled.h1`
   font-size: 2.5rem;
   margin: 0;
   margin-top: 45px;
+  color: ${mainColor};
+  user-select: none;
 `;
 
 export const HeaderDescription = styled.h2`
   font-family: 'Wire One', sans-serif;
-  font-weight: lighter;
+  font-weight: bold;
   font-size: 1.7rem;
   text-align: center;
   margin: 0;
   margin-top: -20px;
+  color: ${mainColor};
+  user-select: none;
 `;
 
 export const LoginSection = styled.div`
@@ -48,17 +90,23 @@ export const LoginSection = styled.div`
   & > div > input {
     color: #2369a1;
     background: #eee;
-    border: none;
+    border: 1px solid #bbbbbb;
     border-radius: 10px;
   }
 
   & > a {
     font-size: 0.9rem;
     padding-bottom: 30px;
+    color: ${mainColor};
   }
 
   & > a.criar-conta {
     margin: 0 auto;
     font-size: 0.8rem;
+    color: ${mainColor};
+  }
+
+  & > button:hover {
+    background: #2ac954;
   }
 `;
